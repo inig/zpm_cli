f9fba6da17cdfa67f6bf55543af028412832f5b7
梁山 <liangshan@192.168.0.101>
2017-11-19 00:18:58 +0800

    脚手架新增 添加插件功能

--- bin/zpm.js
+++ bin/zpm.js
@@ -48,38 +48,139 @@ const validateName = function (input) {
   done(null, true);
 };
 
+const isEmptyObj = function (obj) {
+  var t;
+  for (t in obj)
+    return !1
+  return !0
+};
+
 const getAllPlugins = function () {
   const sep = path.sep;
   let templatesPath = path.resolve(__dirname, `.${sep}plugins`);
   let filenames = fs.readdirSync(templatesPath);
 
   let allPlugins = [];
+  let allPluginsConfig = [];
   let pluginChoice = [];
   filenames.forEach(function (fname) {
     let _realFilePath = path.join(templatesPath, fname);
     let _stat = fs.statSync(_realFilePath);
-    if (_stat.isFile()) {
-      let _name = path.basename(fname, '.js');
-      allPlugins.push(_name);
-      pluginChoice.push({
-        name: _name,
-        value: _name
-      })
+    if (_stat.isDirectory()) {
+      let _realFiles = fs.readdirSync(_realFilePath);
+      if (_realFiles.indexOf('index.js') > -1) {
+        // 存在index.js，则认为是有效的插件
+        allPlugins.push(fname);
+        pluginChoice.push({
+          name: fname,
+          value: fname
+        });
+        let _config = {};
+        try {
+          _config = require(path.join(_realFilePath, 'package.json'));
+        } catch (err) {
+          _config = {};
+        }
+        allPluginsConfig.push({
+          name: fname,
+          value: _config
+        });
+      }
     }
   });
-  return [allPlugins, pluginChoice];
+  return [allPlugins, pluginChoice, allPluginsConfig];
+};
+
+const pluginsCommandList = function (opts) {
+  let allPluginsConfig = opts.allPluginsConfig;
+  for (let i = 0; i < allPluginsConfig.length; i++) {
+    if (i === 0) { console.log(' '); }
+
+    if (isEmptyObj(allPluginsConfig[i].value)) {
+      console.log(`${styles.green}`, `- ${allPluginsConfig[i].name}`);
+    } else {
+      console.log(`${styles.green}`, `- ${allPluginsConfig[i].name}: ${allPluginsConfig[i].value.description}`);
+      console.log(`${styles.black}`, `   Author: ${allPluginsConfig[i].value.author.name} ${allPluginsConfig[i].value.author.email}`);
+      console.log(`${styles.grey}`, `   Latest: v${allPluginsConfig[i].value.version}`);
+    }
+
+    if (i === allPluginsConfig.length - 1) { console.log(' '); }
+  }
+  // console.log(`${styles.grey}`, '\n   所有命令如下：');
+  // console.log(`${styles.grey}`, '\n     └── add   (添加一个插件): zpm plugins add 插件名');
+  // console.log(`${styles.grey}`, '\n     └── list  (显示所有插件): zpm plugins list\n');
+};
+
+const pluginsCommandAdd = function (opts) {
+  const sep = path.sep;
+  let questions = [];
+  let pluginInfo = {
+    plugins: []
+  };
+  // 所有插件
+  let plugins = opts.plugins;
+  // 待安装的插件列表
+  let newPlugins = opts.args.slice(4);
+  // 不存在的插件
+  let illegalPlugins = [];
+  // 存在的插件
+  let legalPlugins = [];
+  newPlugins.forEach(function (p) {
+    if (plugins.indexOf(p) < 0) {
+      illegalPlugins.push(p);
+    } else {
+      legalPlugins.push(p);
+    }
+  });
+  if (legalPlugins.length < 1) {
+    // 无合法的插件
+    questions = [
+      {
+        type: 'checkbox',
+        name: 'plugins',
+        message: '请选择待安装的插件',
+        choices: opts.pluginsChoice
+      }
+    ]
+  } else {
+
+  }
+  if (illegalPlugins.length > 0) {
+    // 不正确的插件名
+    console.log(`${styles.magenta}`, `\n  插件 ${illegalPlugins.join('、')} 不存在！\n`);
+  }
+  prompt(questions).then(function (answers) {
+    if (answers.plugins) {
+      pluginInfo.plugins = answers.plugins;
+      legalPlugins = answers.plugins;
+    }
+    let realPath = path.resolve(process.cwd(), './plugins');
+    fs.exists(realPath, function (exists) {
+      if (!exists) {
+        shelljs.exec(`mkdir ${realPath}`);
+      }
+      let successInstalled = [];
+      let failInstalled = [];
+      legalPlugins.forEach(function (plugin) {
+        try {
+          shelljs.cp('-R', `${path.join(__dirname, '.' + sep)}plugins${sep}${plugin}${sep}index.js`, `${realPath}${sep}${plugin}.js`);
+          successInstalled.push(plugin);
+        } catch (err) {
+          failInstalled.push(plugin);
+        }
+      });
+      if (successInstalled.length > 0) {
+        console.log(`${styles.green}`, `\n  插件 ${successInstalled.join('、')} 添加成功\n`);
+      }
+      if (failInstalled.length > 0) {
+        console.log(`${styles.magenta}`, `\n  插件 ${failInstalled.join('、')} 添加失败\n`);
+      }
+    });
+  });
 };
 
 program
-  .version(`${pkg.version}`)
-
-// program
-  // .command('help')
-  // .description('还没有帮助文件')
-  // .option('-a, --all', 'Whether to display hidden files')
-  // .action(function (options) {
-  //   console.log(process.argv)
-  // })
+  .version(`${pkg.version}`);
 
 program
   .command('init')
@@ -112,8 +213,7 @@ program
     });
 
     let args = process.argv;
-
-    if (allTemplates.indexOf(args[3]) < 0) {
+    if (allTemplates.indexOf(args[3]) < 0 && args.length > 3) {
       console.log(`${styles.red}`, `\n 模板 ${args[3]} 不存在\n`);
       questions = [
         {
@@ -220,24 +320,43 @@ program
   .command('plugins')
   .description('添加插件：zpm plugins [, 插件名]')
   .action(function () {
-    let sep = path.sep;
     let args = process.argv;
-    let questions = [];
-    let pluginInfo = {
-      plugins: []
-    };
+    let tul = getAllPlugins();
+    if (tul[0].length < 1) {
+      console.log(`${styles.magenta}`, '\n  暂时未实现任何插件\n');
+      return;
+    }
     if (args.length < 4) {
-      console.log(`${styles.magenta}`, '\n   命令有误');
       console.log(`${styles.grey}`, '\n   所有命令如下：');
-      console.log(`${styles.grey}`, '\n     └── add (添加一个插件): zpm plugins add 插件名');
-    } else if (args.length < 5) {
-      let tul = getAllPlugins();
-      if (tul[0].length < 1) {
-        // 无可用插件
-        console.log(`${styles.magenta}`, '\n   无可用插件\n');
+      console.log(`${styles.grey}`, '\n     - add : 添加插件，可以同时添加多个插件，以空格区分');
+      console.log(`${styles.grey}`, '\n             zpm plugins add 插件名 [, 插件名]');
+      console.log(`${styles.grey}`, '\n     - list: 显示所有插件');
+      console.log(`${styles.grey}`, '\n             zpm plugins list\n');
+      // pluginsCommandList();
+    } else {
+      switch (args[3]) {
+        case 'list':
+        case 'ls':
+          pluginsCommandList({
+            allPluginsConfig: tul[2]
+          });
+          break;
+        case 'add':
+          pluginsCommandAdd({
+            args: args,
+            plugins: tul[0],
+            pluginsChoice: tul[1]
+          });
+          break;
+        default:
+          console.log(`${styles.magenta}`, '\n   命令有误');
+          console.log(`${styles.grey}`, '\n   所有命令如下：');
+          console.log(`${styles.grey}`, '\n     - add : 添加插件，可以同时添加多个插件，以空格区分');
+          console.log(`${styles.grey}`, '\n             zpm plugins add 插件名 [, 插件名]');
+          console.log(`${styles.grey}`, '\n     - list: 显示所有插件');
+          console.log(`${styles.grey}`, '\n             zpm plugins list\n');
+          break;
       }
-      // console.log('.... all plugins: ', tul[0]);
-      // console.log('.... all plugin choice: ', tul[1]);
     }
   });
 
